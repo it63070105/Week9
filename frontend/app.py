55f9500d6fae4c45260db6216b8b7bb77788d49e
it63070160 <63070160@kmitl.ac.th>
2023-03-24 21:56:15 +0700

    edited app, index for image processing

--- frontend/app.py
+++ frontend/app.py
@@ -1,8 +1,29 @@
 import os
 from flask import Flask, render_template
+import requests
+import base64
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+import json
 
 app = Flask(__name__,template_folder="")
 
+# encode image as base64 string
+def encode_image(image):
+    _, encoded_image = cv2.imencode(".jpg", image)
+    return "data:image/jpeg;base64," + base64.b64encode(encoded_image).decode()
+
+# decode base64 string to image
+def decode_image(image_string):
+    encoded_data = image_string.split(',')[1]
+    nparr = np.frombuffer(base64.b64decode(encoded_data), np.uint8)
+    return cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+
+
+image_file = 'myPicture.jpg'
+url        = "http://3.93.181.229:8088"
 
 @app.route('/')
 def home():
@@ -14,7 +35,41 @@ def home():
 
 @app.route('/<string:name>')
 def template(name):
-    return render_template("index.html", color=name)
+    # Load the image
+    image = cv2.imread(name)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image_string = encode_image(image)
+
+    payload = {
+        "image": image_string,
+        "name": "John",
+        "surname": "Doe",
+        "numbers": [1, 2, 3, 4, 5]
+    }
+
+    response = requests.post(f"{url}/process-image", json=payload)
+    data = json.loads(response.content)
+
+    processed_image_string = data["processed_image"]
+    processed_image = decode_image(processed_image_string)
+
+    # Create a figure and set the title
+    fig = plt.figure(figsize=(12, 4))
+    fig.suptitle('Comparison of Images')
+
+    # Add the first image to the left subplot
+    ax1 = fig.add_subplot(1, 2, 1)
+    ax1.imshow(image)
+    ax1.set_title('Original image')
+
+    # Add the second image to the right subplot
+    ax2 = fig.add_subplot(1, 2, 2)
+    ax2.imshow(processed_image)
+    ax2.set_title('Processed image')
+
+    # Show the plot
+    plt.show()
+    return render_template("index.html", image=plt.show())
     
 
 if __name__ == '__main__':
