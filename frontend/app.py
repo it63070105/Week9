3a00f7b40dbfbc3fd291744a2a26274681aab796
it63070160 <63070160@kmitl.ac.th>
2023-03-24 21:43:40 +0700

    return frontend files to default

--- frontend/app.py
+++ frontend/app.py
@@ -7,16 +7,15 @@ app = Flask(__name__,template_folder="")
 @app.route('/')
 def home():
    
-   env_var_image = os.environ['APP_IMAGE']
+   env_var_colour = os.environ['APP_COLOR']
    
-   return render_template("index.html", image="building.jpg")
+   return render_template("index.html", color= env_var_colour)
     
 
 @app.route('/<string:name>')
 def template(name):
-    return render_template("index.html", image=name)
+    return render_template("index.html", color=name)
     
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0",port="8081")
-
